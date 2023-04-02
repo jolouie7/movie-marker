9f3315d15ae675fee63e4991e2d699293ad3f6ac
Joseph <joseph161216@gmail.com>
2023-04-01 23:48:27 -0700

    Add useDebounce custom hook

---
+++ src/common/hooks/useDebounce.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const useDebounce = (value: string, delay: number = 2000) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Cancel the timeout if value changes (also on delay change or unmount)
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
