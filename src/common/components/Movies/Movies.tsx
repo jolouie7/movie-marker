f8645a676bbecd495f595994eb8901daf532ba17
Joseph <joseph161216@gmail.com>
2023-04-05 13:37:33 -0700

    Move handleAddBookmark up in the file

--- src/common/components/Movies/Movies.tsx
+++ src/common/components/Movies/Movies.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { Movie } from "@/common/types/types";
@@ -8,9 +8,40 @@ import useDebounce from "@/common/hooks/useDebounce";
 
 interface MoviesProps {
   searchTerm: string;
+  page: string;
 }
 
-function Movies({ searchTerm }: MoviesProps) {
+const handleAddBookmark = (movie: Movie) => {
+  console.log("movie: ", movie.imdbID);
+  const favoriteMovie: Movie = {
+    Title: movie.Title,
+    Year: movie.Year,
+    Type: movie.Type,
+    Poster: movie.Poster,
+    imdbID: movie.imdbID,
+    watched: false,
+  };
+  const movies: Movie[] = JSON.parse(
+    localStorage.getItem("localStorageMovies") || "[]"
+  );
+  // If 'movie' is not in localStorage, add it
+  if (movies.length === 0) {
+    localStorage.setItem("localStorageMovies", JSON.stringify([favoriteMovie]));
+  }
+  // If 'movie' is in localStorage, add current movie to the array
+  else {
+    if (
+      !movies.some((storedMovie: Movie) => storedMovie.imdbID === movie.imdbID)
+    ) {
+      movies.push(favoriteMovie);
+      localStorage.setItem("localStorageMovies", JSON.stringify(movies));
+    } else {
+      console.log("Movie already exists in Bookmarks.");
+    }
+  }
+};
+
+function Movies({ searchTerm, page }: MoviesProps) {
   const debouncedSearchQuery = useDebounce(searchTerm, 2000);
 
   const getMovies = async () => {
@@ -29,6 +60,7 @@ function Movies({ searchTerm }: MoviesProps) {
           },
         }
       );
+      console.log("response.data.Search: ", response.data.Search);
       return response.data.Search;
     } catch (error) {
       console.error(error);
@@ -39,43 +71,9 @@ function Movies({ searchTerm }: MoviesProps) {
     ["movies", debouncedSearchQuery],
     getMovies
   );
+
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
-
-  const handleAddFavorite = (movie: Movie) => {
-    console.log("movie: ", movie.imdbID);
-    const favoriteMovie: Movie = {
-      Title: movie.Title,
-      Year: movie.Year,
-      Type: movie.Type,
-      Poster: movie.Poster,
-      imdbID: movie.imdbID,
-      watched: false,
-    };
-    const movies: Movie[] = JSON.parse(
-      localStorage.getItem("localStorageMovies") || "[]"
-    );
-    // If 'movie' is not in localStorage, add it
-    if (movies.length === 0) {
-      localStorage.setItem(
-        "localStorageMovies",
-        JSON.stringify([favoriteMovie])
-      );
-    }
-    // If 'movie' is in localStorage, add current movie to the array
-    else {
-      if (
-        !movies.some(
-          (storedMovie: Movie) => storedMovie.imdbID === movie.imdbID
-        )
-      ) {
-        movies.push(favoriteMovie);
-        localStorage.setItem("localStorageMovies", JSON.stringify(movies));
-      } else {
-        console.log("Movie already exists in favorites.");
-      }
-    }
-  };
 
   return (
     <Box>
@@ -84,7 +82,7 @@ function Movies({ searchTerm }: MoviesProps) {
           Search Movies
         </Flex>
       ) : (
-        <DisplayMovies movies={data} handleAddFavorite={handleAddFavorite} />
+        <DisplayMovies movies={data} handleAddBookmark={handleAddBookmark} />
       )}
     </Box>
   );
