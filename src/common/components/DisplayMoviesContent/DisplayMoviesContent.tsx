58aa8dbbf8e920ddd58d9e245f52642222402b23
Joseph <joseph161216@gmail.com>
2023-04-10 00:07:42 -0700

    Add popover for removal confirmation

--- src/common/components/DisplayMoviesContent/DisplayMoviesContent.tsx
+++ src/common/components/DisplayMoviesContent/DisplayMoviesContent.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { VStack, Image, Box, Text, Flex, Wrap, Button } from "@chakra-ui/react";
+import {
+  VStack,
+  Image,
+  Box,
+  Text,
+  Flex,
+  Wrap,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { Movie } from "@/common/types/types";
-
 interface DisplayMoviesContentProps {
   movies: Movie[];
   handleAddBookmark?: (movie: Movie) => void;
@@ -21,6 +35,11 @@ const DisplayMoviesContent = ({
   const handleClickBookmark = isBookmarkRoute
     ? handleRemoveBookmark
     : handleAddBookmark;
+
+  const handleRemove = (movie: any) => {
+    console.log(movie.Title);
+    handleRemoveBookmark!(movie);
+  };
 
   return (
     <Wrap spacing="5" justify="center">
@@ -51,13 +70,47 @@ const DisplayMoviesContent = ({
             </Flex>
             <Flex justify="space-between">
               {displayWatchedButton && movie.watched === false && (
-                <Button size="sm" onClick={() => handleClickBookmark!(movie)}>
+                <Button
+                  size="sm"
+                  colorScheme="green"
+                  onClick={() => handleClickBookmark!(movie)}
+                >
                   {displayWatchedButton}
                 </Button>
               )}
-              <Button size="sm" onClick={() => handleClickBookmark!(movie)}>
-                {isBookmarkRoute ? "Remove" : "Bookmark"}
-              </Button>
+              {isBookmarkRoute ? (
+                <Popover>
+                  <PopoverTrigger>
+                    <Button size="sm" colorScheme="red">
+                      Remove
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverHeader fontWeight="semibold">
+                      Are you sure you want to remove {movie.Title}?
+                    </PopoverHeader>
+                    <PopoverFooter display="flex" justifyContent="flex-end">
+                      <Button
+                        size="sm"
+                        colorScheme="red"
+                        onClick={() => handleRemove(movie)}
+                      >
+                        Remove
+                      </Button>
+                    </PopoverFooter>
+                  </PopoverContent>
+                </Popover>
+              ) : (
+                <Button
+                  size="sm"
+                  colorScheme="blue"
+                  onClick={() => handleAddBookmark!(movie)}
+                >
+                  Bookmark
+                </Button>
+              )}
             </Flex>
           </Box>
         </VStack>
