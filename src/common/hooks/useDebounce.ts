fe1303c09c077b9a04215bbf54ef624e3e990c04
Joseph <joseph161216@gmail.com>
2023-04-09 20:40:41 -0700

    Update default time

--- src/common/hooks/useDebounce.ts
+++ src/common/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useDebounce = (value: string, delay: number = 2000) => {
+const useDebounce = (value: string, delay: number = 1000) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
